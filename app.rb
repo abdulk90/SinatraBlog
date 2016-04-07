f381e1d5fc806d8873266da48f4c2e3e27dc2f09
abdulk90 <abdul.kayyali@gmail.com>
2016-04-07 15:41:06 -0400

    fixed test for post and blog.  Works properly

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative "lib/blog.rb"
 require_relative "lib/post.rb"
 
 @blog = Blog.new
-blog.add_post Post.new()
-blog.add_post Post.new()
+blog.add_post Post.new("Cats", "1", "Meow")
+blog.add_post Post.new("Dogs", "2", "Woof")
 
 get "/" do
 	@posts = blog.posts
